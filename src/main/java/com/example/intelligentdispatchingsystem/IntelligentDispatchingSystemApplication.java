78b4bdd0fb87e1f4b82c6f9ec948a759b7f1e6a6
MyY <973379630@qq.com>
2025-03-31 16:38:45 +0800

    3.31 update

--- src/main/java/com/example/intelligentdispatchingsystem/IntelligentDispatchingSystemApplication.java
+++ src/main/java/com/example/intelligentdispatchingsystem/IntelligentDispatchingSystemApplication.java
@@ -1,6 +1,5 @@
 package com.example.intelligentdispatchingsystem;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
